1de3473f726890da39eb266b9c388ba5aab80839
Jameson Piscitello <piscitello284@gmail.com>
2021-03-01 22:59:40 +0000

    add stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # coin
+a program for turning a money into dollars and save ammounts
---
+++ src/main.py
@@ -0,0 +1,8 @@
+class money:
+  dollars = [100,50,20,10,5,1,0.25,0.10,0.05,0.01]
+file = open('money.txt')
+
+task = file
+print('${:,.2f}'.format(task)
+
+
