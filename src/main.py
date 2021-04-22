ec7c77eb3f8e2157ceab50462952835fec2862be
jameson piscitello <piscitello284@gmail.com>
2021-04-22 15:02:36 -0500

    make it work

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-class money:
-  dollars = [100,50,20,10,5,1,0.25,0.10,0.05,0.01]
-file = open('money.txt')
-
+dollars = [100,50,20,10,5,1,0.25,0.10,0.05,0.01]
+file = float(input("number: "))
 task = file
-print('${:,.2f} into currency is:'.format(task)
-print(task)
-
+total = []
+for x in dollars:
+  total.append(task // x)
+  task %= x
+print(f"{int(total[0])} hundred dollar bill(s)")
+print(f"{int(total[1])} fifty dollar bill(s)")
+print(f"{int(total[2])} twenty doller bill(s)")
+print(f"{int(total[3])} ten doller bill(s)")
+print(f"{int(total[4])} five doller bill(s)")
+print(f"{int(total[5])} one doller bill(s)")
+print(f"{int(total[0])} quarter(s)")
+print(f"{int(total[0])} dimes(s)")
+print(f"{int(total[0])} nickels(s)")
+print(f"{int(total[0])} penny(s)")
